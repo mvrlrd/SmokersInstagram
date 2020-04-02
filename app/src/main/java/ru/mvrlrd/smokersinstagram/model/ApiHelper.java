1387db7e1a72293409a00c53737f5aca2f6c245d
mvrlrd <igrbelykh@gmail.com>
2020-04-02 12:37:37 +0300

    Initial commit

---
+++ app/src/main/java/ru/mvrlrd/smokersinstagram/model/ApiHelper.java
@@ -0,0 +1,33 @@
+package ru.mvrlrd.smokersinstagram.model;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiHelper {
+    private static final String KEY = "";
+    private static final String BASIC_URL = "";
+
+    public Observable<Photos> requestServer() {
+
+        Gson gson = new GsonBuilder()
+                .excludeFieldsWithoutExposeAnnotation()
+                .create();
+
+        GsonConverterFactory gsonConverterFactory = GsonConverterFactory.create(gson);
+
+        IApiService api = new Retrofit.Builder()
+                .baseUrl(BASIC_URL)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(gsonConverterFactory)
+                .build()
+                .create(IApiService.class);
+
+        return api.getPhotos(KEY).subscribeOn(Schedulers.io());
+    }
+}
---
+++ app/src/main/java/ru/mvrlrd/smokersinstagram/view/MainActivity.java
@@ -0,0 +1,15 @@
+package ru.mvrlrd.smokersinstagram.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import ru.mvrlrd.smokersinstagram.R;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
